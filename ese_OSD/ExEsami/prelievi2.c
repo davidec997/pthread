900b1ac9ec8ae10d2eb5aee8533f12b5c5b52bc7
davidec997 <davide.cascio@icloud.com>
2021-12-26 16:48:18 +0100

    minor fixes

--- ese_OSD/ExEsami/prelievi2.c
+++ ese_OSD/ExEsami/prelievi2.c
@@ -30,6 +30,7 @@ sem_t s_pazienti, s_donatori,m;
 int risorsa;
 int lettini_liberi;
 Boolean dottore_libero;
+char * nomi [2] = {"DONATORE", "PAZIENTE"};
 
 void myInit(void)
 {
@@ -41,143 +42,124 @@ void myInit(void)
     dottore_libero = true;
 }
 
-void inizio_prelievo( Boolean tipo){
-    // tipo --> se 0 e' un donatore con max priorita, se 1 e' paziente normale.
+void inizio_prelievo( Boolean donatore){
+    // donatore --> se 0 e' un donatore con max priorita, se 1 e' paziente normale.
     sem_wait(&m);
     // devo controllare se ci sono lettini liberi e se non ci sono donatori in attesa
-    if (tipo){
 
-    }
+    if (!donatore){
+        //sono un paziente normale... devo controllare se ci sono donatori in attesa
+        if (dottore_libero && lettini_liberi > 0 && donatori_blocc <= 0){
+            // il dottore e' libero, ci sono lettini disponibili e non ci sono donatori bloccati
+            //posso fare la post previa
+            dottore_libero = false;
+            lettini_liberi --;
+            sem_post(&s_pazienti);
+            printf("\t\tSono un %s e HO OTTENUTO IL PERMESSO PER FARE IL PRELIEVO\n",nomi[donatore]);
 
-    if( dottore_libero && lettini_liberi > 0){
-        if (tipo){
-            // se sono un paziente normale devo dare precendeza ai donatori
-            if ( donatori_blocc > 0){
+        } else {
+            // una delle 3 condizioni non e'  verificata... mi blocco
+            pazienti_blocc ++;
+            printf("\t\tSono un %s e MI STO BLOCCANDO\n",nomi[donatore]);
 
-            }
         }
-        auto_in_lavaggio ++;
-        lettini_liberi --;
-        sem_post(&s_pazienti); // post previa
-    }else{
-        pazienti_blocc ++;
+
+        sem_post(&m);
+        sem_wait(&s_pazienti);
+        printf("\t\tSono un %s e STO PER FARMI IL PRELIEVO\n",nomi[donatore]);
+
+    } else {
+        // sono un donatore
+        if (dottore_libero && lettini_liberi > 0){
+            dottore_libero = false;
+            lettini_liberi --;
+            sem_post(&s_donatori);
+            printf("\t\tSono un %s e HO OTTENUTO IL PERMESSO PER FARE IL PRELIEVO\n",nomi[donatore]);
+        } else {
+            donatori_blocc ++;
+            printf("\t\tSono un %s e MI STO BLOCCANDO\n",nomi[donatore]);
+
+        }
+
+        sem_post(&m);
+        sem_wait(&s_donatori);
+        printf("\t\tSono un %s e STO PER FARMI IL PRELIEVO\n",nomi[donatore]);
+
     }
-    sem_post(&m);
-    sem_wait(&s_pazienti);
 }
 
-void svegliaAuto (){
-    int auto_da_svegliare = 0;
-    if (pazienti_blocc > POSTI)
-        auto_da_svegliare = POSTI; // se ho piu di NPOSTI auto bloccate, non le sveglio tutte
-    else
-        auto_da_svegliare = pazienti_blocc; // altrimenti posso svegliarle tutte
-
-    while (auto_da_svegliare > 0){
-        pazienti_blocc--;
-        auto_in_lavaggio++;
-        sem_post(&s_pazienti);
-    }
-}
-
-void fineLettura(){
+void finePrelievo () {
     sem_wait(&m);
-    auto_in_lavaggio --;
     lettini_liberi ++;
+    dottore_libero = true;
 
-    if(!auto_in_lavaggio && donatori_blocc > 0){
+    // sveglio donatori bloccati se ce ne sono e poi i pazienti bloccati
+    if ( donatori_blocc > 0){
         sem_post(&s_donatori);
         donatori_blocc --;
-        camper_in_lavaggio ++;
-    }
-    sem_post(&m);
-}
-
-void inizioScrittura(){
-    sem_wait(&m);
-    if(!camper_in_lavaggio && !auto_in_lavaggio){
-        sem_post(&s_donatori);
-        camper_in_lavaggio++;
-    } else{
-        donatori_blocc ++;
-    }
-    sem_post(&m);
-    sem_wait(&s_donatori);
-}
-
-void fineScrittura(){
-    sem_wait(&m);
-    camper_in_lavaggio --;
-    if(pazienti_blocc > 0){
-        //svegliaAuto();
-        while(pazienti_blocc > 0 && lettini_liberi > 0) {
-            pazienti_blocc--;
-            auto_in_lavaggio++;
-            sem_post(&s_pazienti);
-        }
-    } else if (donatori_blocc > 0){
-        donatori_blocc--;
-        camper_in_lavaggio ++;
-        sem_post(&s_donatori);
+        lettini_liberi --;
+        dottore_libero = false;
+    } else if ( pazienti_blocc > 0) {
+        sem_post(&s_pazienti);
+        pazienti_blocc --;
+        lettini_liberi --;
+        dottore_libero = false;
     }
 
     sem_post(&m);
 }
 
-void *eseguiCamper(void *id) {
+
+void *eseguiCliente(void *id) {
     int *pi = (int *) id;
     int *ptr;
     ptr = (int *) malloc(sizeof(int));
-    if (ptr == NULL) {
-        perror("Problemi con l'allocazione di ptr\n");
-        exit(-1);
-    }
-
-    for(int t =1;t<NTIMES;t++){
-        inizioScrittura();
-        //risorsa += 1;
-        printf("[CAMPER %d]  mi sto lavando\n",*pi);
-        sleep(6);
-        fineScrittura();
-    }
-
-    *ptr = *pi;
-    pthread_exit((void *) ptr);
-}
-
-void *eseguiAuto(void *id) {
-    int *pi = (int *) id;
-    int *ptr;
-    ptr = (int *) malloc(sizeof(int));
+    Boolean donatore;
 
     if (ptr == NULL) {
         perror("Problemi con l'allocazione di ptr\n");
         exit(-1);
     }
 
+    donatore = rand() % 2;
+
     for(int t =1;t<NTIMES;t++){
-        inizio_prelievo();
-        printf("[AUTO %d]  mi sto lavando\n",*pi);
+        inizio_prelievo(donatore);
+        printf("[THREAD %d]  sono un %s e volgio farmi un prelievo\n",*pi,nomi[donatore]);
         sleep(3);
-        fineLettura();
+        finePrelievo();
+        printf("[THREAD %d]  sono un %s e ho fatto il prelievo\n",*pi,nomi[donatore]);
+
     }
     *ptr = *pi;
     pthread_exit((void *) ptr);
+}
+
+void *eseguiDottore ( void * id) {
+    /*int *pi = (int *) id;
+    int *ptr;
+    ptr = (int *) malloc(sizeof(int));
+
+    if (ptr == NULL) {
+        perror("Problemi con l'allocazione di ptr\n");
+        exit(-1);
+    }
+
+    *ptr = *pi;
+    pthread_exit((void *) ptr);*/
 }
 
 int main (int argc, char **argv)
 {
-    pthread_t *thread_auto;
-    pthread_t *thread_camper;
-
-    int *taskidsA, *taskidsC;
+    pthread_t *thread;
+    int *taskids;
     int i;
     int *p;
-    int NUM_AUTO, NUM_CAMPER;
+    int NUM_THREADS;
     char error[250];
 
     /* Controllo sul numero di parametri */
-    if (argc != 3 ) /* Devono essere passati 2 parametri */
+    if (argc != 2 ) /* Deve essere passato esattamente un parametro */
     {
         sprintf(error,"Errore nel numero dei parametri %d\n", argc-1);
         perror(error);
@@ -185,78 +167,56 @@ int main (int argc, char **argv)
     }
 
     /* Calcoliamo il numero passato che sara' il numero di Pthread da creare */
-    NUM_AUTO = atoi(argv[1]);
-    NUM_CAMPER = atoi(argv[2]);
-    if (NUM_AUTO <= 0 || NUM_CAMPER <= 0)
+    NUM_THREADS = atoi(argv[1]);
+    if (NUM_THREADS <= 0)
     {
-        sprintf(error, "Errore: Il primo o secondo parametro non e' un numero strettamente maggiore di 0\n");
+        sprintf(error,"Errore: Il primo parametro non e' un numero strettamente maggiore di 0 ma e' %d\n", NUM_THREADS);
         perror(error);
         exit(2);
     }
 
     myInit();
+    srand(555);
 
-    thread_auto=(pthread_t *) malloc((NUM_AUTO) * sizeof(pthread_t));
-    thread_camper=(pthread_t *) malloc((NUM_CAMPER) * sizeof(pthread_t));
-
-    if (thread_auto == NULL || thread_camper == NULL)
+    thread=(pthread_t *) malloc(NUM_THREADS * sizeof(pthread_t));
+    if (thread == NULL)
     {
         perror("Problemi con l'allocazione dell'array thread\n");
         exit(3);
     }
-    taskidsA = (int *) malloc(NUM_AUTO * sizeof(int));
-    taskidsC = (int *) malloc(NUM_CAMPER * sizeof(int));
-
-    if (taskidsA == NULL || taskidsC == NULL)
+    taskids = (int *) malloc(NUM_THREADS * sizeof(int));
+    if (taskids == NULL)
     {
-        perror("Problemi con l'allocazione dell'array taskidsA o taskidsC\n");
+        perror("Problemi con l'allocazione dell'array taskids\n");
         exit(4);
     }
 
-    // CREAZIONE PRIMA DELLE AUTO....
-    for (i=0; i < NUM_AUTO; i++)
+    //genero il trhead 0 dottore
+    pthread_create(&thread[0], NULL, eseguiDottore, (void *) (&taskids[0]));
+
+    for (i=1; i < NUM_THREADS; i++)
     {
-        taskidsA[i] = i;
-        //printf("Sto per creare il thread %d-esimo\n", taskidsA[i]);
-        if (pthread_create(&thread_auto[i], NULL, eseguiAuto, (void *) (&taskidsA[i])) != 0)
+        taskids[i] = i;
+        // printf("Sto per creare il thread %d-esimo\n", taskids[i]);
+        if (pthread_create(&thread[i], NULL, eseguiCliente, (void *) (&taskids[i])) != 0)
         {
-            sprintf(error, "SONO IL MAIN E CI SONO STATI PROBLEMI DELLA CREAZIONE DEL thread %d-esimo\n", taskidsA[i]);
+            sprintf(error,"SONO IL MAIN E CI SONO STATI PROBLEMI DELLA CREAZIONE DEL thread %d-esimo\n", taskids[i]);
             perror(error);
             exit(5);
         }
-        printf("SONO IL MAIN e ho creato il Pthread AUTO %i-esimo con id=%lu\n", i, thread_auto[i]);
-    }
-    for (i= 0; i < NUM_CAMPER ; i++)
-    {
-        taskidsC[i] = i;
-        //printf("Sto per creare il thread %d-esimo\n", taskidsA[i]);
-        if (pthread_create(&thread_camper[i], NULL, eseguiCamper, (void *) (&taskidsC[i])) != 0)
-        {
-            sprintf(error, "SONO IL MAIN E CI SONO STATI PROBLEMI DELLA CREAZIONE DEL thread %d-esimo\n", taskidsA[i]);
-            perror(error);
-            exit(5);
-        }
-        printf("SONO IL MAIN e ho creato il Pthread CAMPER %i-esimo con id=%lu\n", i, thread_camper[i]);
+        //printf("SONO IL MAIN e ho creato il Pthread %i-esimo con id=%lu\n", i, thread[i]);
     }
 
-    sleep(2);
-
-    for (i=0; i < NUM_AUTO; i++)
-    {
-        int *ris;
-        /* attendiamo la terminazione di tutti i thread generati */
-        pthread_join(thread_auto[i], (void**) & p);
-        ris= p;
-        printf("Pthread  %d-esimo restituisce %d\n", i, *ris);
-    }
-    for (i=0; i < NUM_CAMPER; i++)
+    for (i=1; i < NUM_THREADS; i++)
     {
         int ris;
-        pthread_join(thread_camper[i], (void**) & p);
+        /* attendiamo la terminazione di tutti i thread customers */
+        pthread_join(thread[i], (void**) & p);
         ris= *p;
-        printf("Pthread Scrittore %d-esimo restituisce %d\n", i, ris);
+        printf("Pthread %d-esimo restituisce %d  -->numero di volte che si e' tagliato i capelli\n", i, ris);
     }
 
+    //pthread_mutex_destroy(&m);
     exit(0);
 }
 
