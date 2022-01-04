635456a1d93cee0ca7199eeaf82c0f073dd8086e
davidec997 <davide.cascio@icloud.com>
2022-01-04 11:26:07 +0100

    modifie ese19.c

--- Sistemi_Embedded/15-23/ese19.c
+++ Sistemi_Embedded/15-23/ese19.c
@@ -1,206 +1,333 @@
 //
+// Created by davide on 02/01/22.
 //
+#define USA_SEM
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <semaphore.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <semaphore.h>
+#include <stdlib.h>
 
-#define NTIMES 30
+// numero buste della mailbox
+#define N 3
 
-#define N 10 // dimensione coda
+// scrittori
+#define M 8
 
-typedef enum {false, true} Boolean;
-char *receivers [3] = {"RECEIVER A", "RECEIVER B", "RECEIVER C"};
+// lettori
+#define R 5
+
+// il dato contenuto nella busta
 typedef int T;
 
-int valGlobale;
-struct mailbox_t {
-    int* coda_circolare;
-    int head , tail;
-    int n_msg;
-    pthread_cond_t vuota,piena;
-    pthread_mutex_t mtx;
-}mailbox;
+#define NESSUNO -1
 
 
-void init_mailbox(struct mailbox_t *mb){
-
-    pthread_cond_init(&mb->vuota, NULL);
-    pthread_cond_init(&mb->piena, NULL);
-
-    pthread_mutex_init(&mb->mtx, NULL);
-    mb->head = mb->tail = 0;
-    mb->coda_circolare = malloc(N * sizeof (int*));
-    for (int j = 0; j < N; ++j) mb->coda_circolare[j] = (int*)-1;
-    mb->n_msg = 0;
-
-}
+#ifdef USA_SEM
 
 
-void genera_msg(struct mailbox_t *mb){
-    //controllo se c'e' posto nella mailbox
-    pthread_mutex_lock(&mb->mtx);
-    while (mb->n_msg >= N){
-        //aspetto perche' la mailbox e' piena
-        printf("[SERVER] MAILBOX PIENA\n");
-        pthread_cond_wait(&mb->vuota ,&mb->mtx);
-    }
-    //posso generare il msg
-    T msg = valGlobale;
-    valGlobale ++;
-    mb->coda_circolare[mb->head] = msg;
-    printf("[SERVER]\t\tHo appena generato il msg %d e messo in mb[%d]\n",msg,mb->head);
-    mb->head = (mb->head + 1) % N;
-    mb->n_msg ++;
-    pthread_mutex_unlock(&mb->mtx);
-    pthread_cond_signal(&mb->piena);
-}
+struct semaforoprivato_t {
+    sem_t s;
+    int c;
+};
 
-void *generatore (void * id){
-    int *pi = (int *)id;
-    int *ptr;
-    ptr = (int *) malloc( sizeof(int));
-    if (ptr == NULL){
-        perror("Problemi con l'allocazione di ptr\n");
-        exit(-1);
-    }
-    while(1){
-        sleep(1);
-        genera_msg(&mailbox);
-    }
-    *ptr = 0;
-    pthread_exit((void *) ptr);
-}
+struct gestore_t {
+    sem_t mutex;
 
+    int next[N];
+    int head, tail; /* il valore -1 e' utilizzato per terminare la coda */
+    int free;
 
-void leggi (struct mailbox_t *mb, int *pi){
-    T msg;
-    Boolean letto;
-    //leggo
-    //pthread_cond_wait(&mb->nuovo_msg,&mb->mtx);
+    struct semaforoprivato_t priv[N];
 
-    msg = mb->coda_circolare[mb->tail];
-    printf("[RECEIVER %d]\t\t Ho letto il messaggio\t%d dalla posizione\t[%d]\n",*pi,msg,mb->tail);
+    struct semaforoprivato_t ricezione[2];
+};
 
-    mb->tail = (mb->tail + 1) % N;
-    mb->n_msg --;
-
-}
-
-void ricevi (struct mailbox_t *mb, int *pi){
-    //controllo se c' e' qualcosa da leggere
-    //printf("ECCO IL MIO INDICE \t\t\t\t\t%d\n",*pi );
-
-    pthread_mutex_lock(&mb->mtx);
-    while (mb->n_msg <= 0){
-        printf("[RECEIVER %d] MAILBOX VUOTA\n",*pi);
-        pthread_cond_wait(&mb->piena,&mb->mtx);
-    }
-    //leggo
-    leggi(&mailbox,pi);
-
-    pthread_mutex_unlock(&mb->mtx);
-}
-
-void *receiver (void *id) {
-    int *pi = (int *)id;
-    int *ptr;
-    ptr = (int *) malloc( sizeof(int));
-    int n_attraversamenti=0;
-    if (ptr == NULL){
-        perror("Problemi con l'allocazione di ptr\n");
-        exit(-1);
-    }
-
-    while (1){
-        // printf("ECCO IL MIO INDICE \t%d\n",*pi);
-        sleep(3);
-
-        ricevi(&mailbox,pi);
-    }
-    *ptr = 0;
-    pthread_exit((void *) ptr);
-}
-
-
-
-int main (int argc, char **argv)
+void semaforoprivato_init(struct semaforoprivato_t *s)
 {
-    pthread_t *thread;
-    int *taskids;
+    sem_init(&s->s,0,0);
+    s->c = 0;
+}
+
+void gestore_init(struct gestore_t *g)
+{
     int i;
-    int *p;
-    int NUM_THREADS;
-    char error[250];
 
-    /* Controllo sul numero di parametri */
-    if (argc != 2 ) /* Deve essere passato esattamente un parametro */
-    {
-        sprintf(error,"Errore nel numero dei parametri %d\n", argc-1);
-        perror(error);
-        exit(1);
+    /* mutua esclusione */
+    sem_init(&g->mutex,0,1);
+
+    /* semafori privati */
+    for (i=0; i<N; i++)
+        semaforoprivato_init(&g->priv[i]);
+    semaforoprivato_init(&g->ricezione[0]);
+    semaforoprivato_init(&g->ricezione[1]);
+
+
+    /* coda */
+    g->head = g->tail = NESSUNO; // non c'e' nessuno in coda
+
+    /* la coda inizialmente e' concatenata tramite free */
+    g->free = 0;
+    for (i=0; i<N-1; i++)
+        g->next[i] = i+1;
+    g->next[N-1] = NESSUNO;
+}
+
+int gestore_richiedi_busta_vuota(struct gestore_t *g, int prio)
+{
+    int miblocco;
+    int bustavuota;
+
+    sem_wait(&g->mutex);
+
+    /* devo capire se posso accedere alla coda */
+    switch (prio) {
+        case 0:
+            miblocco = g->priv[0].c;
+            break;
+
+        case 1:
+            miblocco = g->priv[0].c || g->priv[1].c;
+            break;
+
     }
 
-    /* Calcoliamo il numero passato che sara' il numero di Pthread da creare */
-    NUM_THREADS = atoi(argv[1]);
-    if (NUM_THREADS <= 0)
-    {
-        sprintf(error,"Errore: Il primo parametro non e' un numero strettamente maggiore di 0 ma e' %d\n", NUM_THREADS);
-        perror(error);
-        exit(2);
+    if (miblocco || g->free == NESSUNO) {
+        g->priv[prio].c++;
+        sem_post(&g->mutex);
+        sem_wait(&(g->priv[prio].s));
+        g->priv[prio].c--;
     }
 
-    init_mailbox(&mailbox);
+    bustavuota = g->free;       // indice busta vuota
+    g->free = g->next[g->free];
 
-    thread=(pthread_t *) malloc(NUM_THREADS * sizeof(pthread_t));
-    if (thread == NULL)
-    {
-        perror("Problemi con l'allocazione dell'array thread\n");
-        exit(3);
-    }
-    taskids = (int *) malloc(NUM_THREADS * sizeof(int));
-    if (taskids == NULL)
-    {
-        perror("Problemi con l'allocazione dell'array taskids\n");
-        exit(4);
-    }
+    sem_post(&g->mutex);
+    return bustavuota;
+}
 
+void gestore_accoda_busta_piena(struct gestore_t *g, int b)
+{
+    sem_wait(&g->mutex);
 
-    srand(555);
+    /* inserisco una busta in coda */
+    g->next[b] = NESSUNO;
+    if (g->head == NESSUNO)
+        g->head = b;
+    else
+        g->next[g->tail] = b;
+    g->tail = b;
 
-    sleep(2);
+    if (g->ricezione[0].c)              // no ...
+        sem_post(&g->ricezione[0].s);
+    else
+        sem_post(&g->mutex);
+}
 
-    for (i=0; i < NUM_THREADS; i++)
-    {
-        taskids[i] = i;
-        if (i == NUM_THREADS -1){
+int gestore_estrai_busta_piena(struct gestore_t *g, int prio)
+{
+    int bustaestratta;
 
-            pthread_create(&thread[i], NULL, generatore, (void *) (&taskids[i]));
-        } else {
-            if (pthread_create(&thread[i], NULL, receiver, (void *) (&taskids[i])) != 0)
-            {
-                sprintf(error,"SONO IL MAIN E CI SONO STATI PROBLEMI DELLA CREAZIONE DEL thread %d-esimo\n", taskids[i]);
-                perror(error);
-                exit(5);
-            }
+    sem_wait(&g->mutex);
+
+    if (prio){// senza prio
+        if (g->head == NESSUNO) {
+            g->ricezione[1].c++;
+            sem_post(&g->mutex);
+            sem_wait(&g->ricezione[1].s);
+            g->ricezione[1].c--;
         }
 
-        //printf("SONO IL MAIN e ho creato il Pthread %i-esimo con id=%lu\n", i, thread[i]);
+    }else { // con prio
+        if (g->head == NESSUNO) {
+            g->ricezione[0].c++;
+            sem_post(&g->mutex);
+            sem_wait(&g->ricezione[0].s);
+            g->ricezione[0].c--;
+        }
     }
 
-    for (i=0; i < NUM_THREADS; i++)
-    {
-        int ris;
-        /* attendiamo la terminazione di tutti i thread generati */
-        pthread_join(thread[i], (void**) & p);
-        ris= *p;
-        printf("Pthread %d-esimo restituisce %d\n", i, ris);
-    }
+    /* estraggo una busta */
+    bustaestratta = g->head;
+    g->head = g->next[g->head];
 
-    exit(0);
+    sem_post(&g->mutex);
+
+    return bustaestratta;
+}
+
+void gestore_rilascio_busta_vuota(struct gestore_t *g, int b)
+{
+    sem_wait(&g->mutex);
+
+    // rilascio la busta
+    g->next[b] = g->free;
+    g->free = b;
+
+    // sveglio in modo prioritario
+    if (g->priv[0].c)
+        sem_post(&g->priv[0].s);
+    else if (g->priv[1].c)
+        sem_post(&g->priv[1].s);
+    else if (g->priv[2].c)
+        sem_post(&g->priv[2].s);
+    else
+        sem_post(&g->mutex);
+}
+
+#endif
+
+
+/* mailbox */
+
+/* la mailbox e' la struttura dati condivisa che permette di inviare e
+   ricevere messaggi. la sua struttura dati condivisa b e' un array di
+   buste che contengono un tipo T */
+
+struct busta_t {
+    T data;
+} gestore;
+
+
+struct mailbox_t {
+    struct busta_t b[N];
+    struct gestore_t G;
+} mailbox;
+
+
+
+/* inizializzazione della struttura condivisa */
+void init_mailbox(struct mailbox_t *m)
+{
+    gestore_init(&m->G);
+
+    /* eventuale inizializzazione di tutti gli elementi di b */
+}
+
+void send(struct mailbox_t *m, T msg, int prio)
+{
+    int bustadariempire;
+
+    bustadariempire = gestore_richiedi_busta_vuota(&m->G, prio);
+    m->b[bustadariempire].data = msg;
+    fprintf(stderr, "  s %8d\n", msg);
+    gestore_accoda_busta_piena(&m->G, bustadariempire);
+}
+
+T receive(struct mailbox_t *m, 0)
+{
+    int bustadaritornare;
+    T dato;
+
+    bustadaritornare = gestore_estrai_busta_piena(&m->G);
+    dato = m->b[bustadaritornare].data;
+    fprintf(stderr, "   r %7d\n", dato);
+    gestore_rilascio_busta_vuota(&m->G, bustadaritornare);
+
+    return dato;
+}
+
+T receiveP(struct mailbox_t *m)
+{
+    int bustadaritornare;
+    T dato;
+
+    bustadaritornare = gestore_estrai_busta_piena(&m->G, 0);
+    dato = m->b[bustadaritornare].data;
+    fprintf(stderr, "   r %7d\n", dato);
+    gestore_rilascio_busta_vuota(&m->G, bustadaritornare);
+
+    return dato;
 }
 
 
+/* ------------------------------- */
+
+/* alla fine di ogni ciclo ogni thread aspetta un po'.
+   Cosa succede se tolgo questa nanosleep?
+   di fatto solo i thread di tipo B riescono ad entrare --> starvation!!!!
+   (provare per credere)
+*/
+void pausetta(void)
+{
+    struct timespec t;
+    t.tv_sec = 0;
+    t.tv_nsec = (rand()%10+1)*1000000;
+    nanosleep(&t,NULL);
+}
+
+// un contatore
+int cont;
+
+/* i thread */
+
+
+void *mittente(void *arg)
+{
+    int p = (int)arg; // Priorita'
+    T i;
+
+    for (;;) {
+        i = ++cont;
+        fprintf(stderr, "S %4d%6d\n", p, i);
+        send(&mailbox, cont, p);
+        pausetta();
+    }
+    return 0;
+}
+
+void *ricevente(void *arg)
+{
+    T i;
+
+    for (;;) {
+        if (rand()%2 ==0){
+            printf("[RECEIVE P] INVOCATA\n");
+            i= receiveP(receive(&mailbox));
+        } else{
+            printf("[RECEIVE N] INVOCATA\n");
+            i= receive(receive(&mailbox));
+        }
+
+        //fprintf(stderr, " R%10d\n", i);
+        pausetta();
+    }
+    return 0;
+}
+
+/* la creazione dei thread */
+
+int main()
+{
+    pthread_attr_t a;
+    pthread_t p;
+    int i;
+
+    /* inizializzo il mio sistema */
+    init_mailbox(&mailbox);
+
+    /* inizializzo i numeri casuali, usati nella funzione pausetta */
+    srand(555);
+
+    pthread_attr_init(&a);
+
+    /* non ho voglia di scrivere 10000 volte join! */
+    pthread_attr_setdetachstate(&a, PTHREAD_CREATE_DETACHED);
+
+    for (i=0; i<M; i++)
+        pthread_create(&p, &a, mittente, (void *)(rand()%3));
+
+    for (i=0; i<R; i++)
+        pthread_create(&p, &a, ricevente, NULL);
+
+
+    pthread_attr_destroy(&a);
+
+    /* aspetto 10 secondi prima di terminare tutti quanti */
+    sleep(3);
+
+    return 0;
+}
 
 
