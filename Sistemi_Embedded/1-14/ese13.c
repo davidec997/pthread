e08c4c81988c648423b85fe24f5677b84e4ac494
davidec997 <davide.cascio@icloud.com>
2022-01-05 17:29:05 +0100

    fixed ese13.c

--- Sistemi_Embedded/1-14/ese13.c
+++ Sistemi_Embedded/1-14/ese13.c
@@ -1,8 +1,4 @@
-//
-// Created by davide on 04/01/22.
-//
-//
-//
+//OK
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -14,7 +10,6 @@
 #define N 10 // dimensione coda
 
 typedef enum {false, true} Boolean;
-char *receivers [3] = {"RECEIVER A", "RECEIVER B", "RECEIVER C"};
 typedef int T;
 
 int valGlobale;
@@ -24,18 +19,20 @@ struct mailbox_t {
     int n_msg, turno;
     int conferma_lettura [2];
     //pthread_cond_t vuota,piena;
-    sem_t vuota, piena, letto_da_tutti;
+    sem_t vuota, piena, letto_da_tutti, *s_turno;
     pthread_mutex_t mtx;
 }mailbox;
 
 
 void init_mailbox(struct mailbox_t *mb){
 
-    //pthread_cond_init(&mb->vuota, NULL);
-    //pthread_cond_init(&mb->piena, NULL);
     sem_init(&mb->vuota,0,0);
     sem_init(&mb->piena,0,0);
     sem_init(&mb->letto_da_tutti,0,0);
+    mb->s_turno = malloc(2 * sizeof (sem_t));
+    sem_init(&mb->s_turno[0],0,1);
+    sem_init(&mb->s_turno[1],0,0);
+
 
 
     pthread_mutex_init(&mb->mtx, NULL);
@@ -48,7 +45,7 @@ void init_mailbox(struct mailbox_t *mb){
 
     mb->conferma_lettura[0] = 0;
     mb->conferma_lettura[1] = 0;
-
+    mb->turno = 0;
 }
 
 
@@ -58,11 +55,12 @@ void genera_msg(struct mailbox_t *mb){
     pthread_mutex_lock(&mb->mtx);
 
     if (mb->n_msg > N){
-       // non c'e' posto
-       pthread_mutex_unlock(&mb->mtx);
-       sem_wait(&mb->vuota); // mi blocco
-       pthread_mutex_lock(&mb->mtx);
-   }
+        // non c'e' posto
+        mb->b_piena ++;
+        pthread_mutex_unlock(&mb->mtx);
+        sem_wait(&mb->vuota); // mi blocco
+        pthread_mutex_lock(&mb->mtx);
+    }
 
     //posso generare il msg
     T msg = valGlobale;
@@ -72,31 +70,12 @@ void genera_msg(struct mailbox_t *mb){
     mb->head = (mb->head + 1) % N;
     mb->n_msg ++;
 
-   // sem_post(&mb->piena);
-   if (mb->b_vota > 0){
-       mb->b_vota --;
-       sem_post(&mb->piena);
-   }
+    sem_post(&mb->piena);
+
     pthread_mutex_unlock(&mb->mtx);
 
 }
 
-void aspetta_lettura(struct mailbox_t *mb){
-    pthread_mutex_lock(&mb->mtx);
-    printf("[SERVER]\t\tSto aspettando la lettura dei receivers:  [%d]  [%d]\n",mb->conferma_lettura[0], mb->conferma_lettura[1]);
-
-    pthread_mutex_unlock(&mb->mtx);
-    sem_wait(&mb->letto_da_tutti);
-    pthread_mutex_lock(&mb->mtx);
-
-    printf("[SERVER]\t\tHanno letto tutti i receivers:  [%d]  [%d]\n",mb->conferma_lettura[0], mb->conferma_lettura[1]);
-
-    mb->conferma_lettura[0] = 0;
-    mb->conferma_lettura[1] = 0;
-    pthread_mutex_unlock(&mb->mtx);
-
-
-}
 
 void *generatore (void * id){
     int *pi = (int *)id;
@@ -115,46 +94,83 @@ void *generatore (void * id){
     pthread_exit((void *) ptr);
 }
 
+void aspetta_lettura(struct mailbox_t *mb , int *pi){
+    //pthread_mutex_lock(&mb->mtx);
+    printf("[RECEIVER %d]\t\tSto aspettando la lettura dei receivers:  [%d]  [%d]\n",*pi,mb->conferma_lettura[0], mb->conferma_lettura[1]);
 
-void leggi (struct mailbox_t *mb, int *pi){
+    pthread_mutex_unlock(&mb->mtx);     // solo il primo entra qui.. lui cambia turno
+    sem_wait(&mb->letto_da_tutti);
+    pthread_mutex_lock(&mb->mtx);
+
+    printf("[SERVER]\t\tHanno letto tutti i receivers:  [%d]  [%d]\n",mb->conferma_lettura[0], mb->conferma_lettura[1]);
+
+    mb->conferma_lettura[0] = 0;
+    mb->conferma_lettura[1] = 0;
+    pthread_mutex_unlock(&mb->mtx);
+
+}
+
+void leggi (struct mailbox_t *mb, int *pi) {
     T msg;
     //leggo
 
     msg = mb->coda_circolare[mb->tail];
-    printf("[RECEIVER %d]\t\t Ho letto il messaggio\t%d dalla posizione\t[%d]\n",*pi,msg,mb->tail);
+    printf("[RECEIVER %d]\t\t Ho letto il messaggio\t%d dalla posizione\t[%d]\n", *pi, msg, mb->tail);
 
     mb->conferma_lettura[*pi] = 1;
+    // turno
+    if (mb->turno == 0) {
+        mb->turno = 1;
+        sem_post(&mb->s_turno[1]);
+    }
+    /*} else {
+        mb->turno = 0;
+        sem_post(&mb->s_turno[0]);
+    }*/
 
     // se hanno letto entrambi --> signal su letto_da_tutti
-    if (mb->conferma_lettura[0] && mb->conferma_lettura[1]){
+    if (mb->conferma_lettura[0] && mb->conferma_lettura[1]) {
         mb->tail = (mb->tail + 1) % N;
-        mb->n_msg --;
-        //sem_post(&mb->vuota);
+        mb->n_msg--;
+        if (mb->b_piena) {
+            sem_post(&mb->vuota);
+            mb->b_piena--;
+        }
         sem_post(&mb->letto_da_tutti);
+        mb->turno = 0;                      // solo il secondo entra qui.. lui cambia turno
+        sem_post(&mb->s_turno[0]);
+    } else {
+        aspetta_lettura(&mailbox, pi);
     }
 
 }
 
+
 void ricevi (struct mailbox_t *mb, int *pi){
     //controllo se c' e' qualcosa da leggere
-    printf("ECCO IL MIO INDICE \t\t\t%d\n",*pi );
+    //printf("ECCO IL MIO INDICE \t\t\t%d\n",*pi );
 
     pthread_mutex_lock(&mb->mtx);
-    /*while (mb->n_msg <= 0){
-        printf("[RECEIVER %d] MAILBOX VUOTA\n",*pi);
-        pthread_cond_wait(&mb->piena,&mb->mtx);
-    }*/
+
     if (mb->n_msg <= 0){
         // mi blocco
         printf("[RECEIVER %d] MAILBOX VUOTA\n",*pi);
         mb->b_vota ++;
 
-       // pthread_mutex_unlock(&mb->mtx);
+        pthread_mutex_unlock(&mb->mtx);
+        if (*pi != 0) sem_wait(&mb->piena);
+        pthread_mutex_lock(&mb->mtx);
     }
 
-    pthread_mutex_unlock(&mb->mtx);
-    sem_wait(&mb->piena);
-    pthread_mutex_lock(&mb->mtx);
+    if (*pi == 0){
+        pthread_mutex_unlock(&mb->mtx);
+        sem_wait(&mb->s_turno[0]);
+        pthread_mutex_lock(&mb->mtx);
+    }else{
+        pthread_mutex_unlock(&mb->mtx);
+        sem_wait(&mb->s_turno[1]);
+        pthread_mutex_lock(&mb->mtx);
+    }
 
 
     printf("[RECEIVER %d] LEGGO...\n",*pi);
@@ -174,12 +190,13 @@ void *receiver (void *id) {
         perror("Problemi con l'allocazione di ptr\n");
         exit(-1);
     }
+    sleep(2);
 
     while (1){
         // printf("ECCO IL MIO INDICE \t%d\n",*pi);
         sleep(2);
         ricevi(&mailbox,pi);
-        aspetta_lettura(&mailbox);
+       // aspetta_lettura(&mailbox);
     }
     *ptr = 0;
     pthread_exit((void *) ptr);
